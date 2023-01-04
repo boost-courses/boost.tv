3714b99dc9493a3133c17a3caba2828467d4e1ab
Bulatov Viacheslav <bulatov.vyacheslav@gmail.com>
2023-01-04 18:42:24 +0100

    Lesson-2 Adding Router and SplashScreen with Main page

--- com.domain.app.boosttv/.eslintrc.js
+++ com.domain.app.boosttv/.eslintrc.js
@@ -5,33 +5,33 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  plugins: ['prettier'],
-  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'prettier'],
+  plugins: ["prettier"],
+  extends: ["eslint:recommended", "plugin:prettier/recommended", "prettier"],
   rules: {
-    quotes: [2, 'single', 'avoid-escape'],
-    semi: [2, 'never'],
-    'no-extra-boolean-cast': 'off',
-    'no-unused-vars': [
+    quotes: [2, "double", "avoid-escape"],
+    semi: [2, "always"],
+    "no-extra-boolean-cast": "off",
+    "no-unused-vars": [
       1,
       {
         ignoreRestSiblings: true,
-        argsIgnorePattern: 'res|next|^err',
+        argsIgnorePattern: "res|next|^err",
       },
     ],
-    'prettier/prettier': [
-      'error',
+    "prettier/prettier": [
+      "error",
       {
-        trailingComma: 'all',
-        singleQuote: true,
+        singleQuote: false,
         tabWidth: 2,
-        semi: false,
+        semi: true,
         printWidth: 100,
+        parser: "flow",
       },
     ],
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint",
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module",
   },
-}
+};
